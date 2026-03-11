bc073fdf44d3dc198b6ac2ab9ad68e12096963f4
ZyberPE <zyberpe@gmail.com>
2026-03-11 16:02:03 -0400

    Update Main.php

--- src/BossBar/Main.php
+++ src/BossBar/Main.php
@@ -8,30 +8,28 @@ use pocketmine\player\Player;
 
 use pocketmine\network\mcpe\protocol\AddActorPacket;
 use pocketmine\network\mcpe\protocol\RemoveActorPacket;
+
 use pocketmine\network\mcpe\protocol\types\entity\EntityIds;
 use pocketmine\network\mcpe\protocol\types\entity\EntityMetadataCollection;
-use pocketmine\network\mcpe\protocol\types\entity\EntityMetadataFlags;
 use pocketmine\network\mcpe\protocol\types\entity\EntityMetadataProperties;
-use pocketmine\network\mcpe\protocol\types\entity\FloatMetadataProperty;
-use pocketmine\network\mcpe\protocol\types\entity\LongMetadataProperty;
-use pocketmine\network\mcpe\protocol\types\entity\StringMetadataProperty;
+use pocketmine\network\mcpe\protocol\types\entity\EntityMetadataFlags;
 
 class Main extends PluginBase{
 
-    private int $entityId = 123456;
+    private int $entityId = 999999;
     private int $messageIndex = 0;
 
-    protected function onEnable(): void{
+    protected function onEnable() : void{
         $this->saveDefaultConfig();
 
         $interval = $this->getConfig()->get("update-time", 5) * 20;
 
-        $this->getScheduler()->scheduleRepeatingTask(new ClosureTask(function(): void{
+        $this->getScheduler()->scheduleRepeatingTask(new ClosureTask(function() : void{
             $this->updateBossBar();
         }), $interval);
     }
 
-    private function updateBossBar(): void{
+    private function updateBossBar() : void{
 
         $messages = $this->getConfig()->get("messages");
 
@@ -49,36 +47,40 @@ class Main extends PluginBase{
         $text = $top . "\n" . $bottom;
 
         foreach($this->getServer()->getOnlinePlayers() as $player){
+            $this->removeBossBar($player);
             $this->sendBossBar($player, $text);
         }
 
         $this->messageIndex++;
     }
 
-    private function sendBossBar(Player $player, string $text): void{
+    private function sendBossBar(Player $player, string $text) : void{
 
         $metadata = new EntityMetadataCollection();
 
         $metadata->setString(EntityMetadataProperties::NAMETAG, $text);
         $metadata->setLong(EntityMetadataProperties::FLAGS, 1 << EntityMetadataFlags::SILENT);
-        $metadata->setFloat(EntityMetadataProperties::HEALTH, 100);
-        $metadata->setFloat(EntityMetadataProperties::MAX_HEALTH, 100);
+        $metadata->setFloat(EntityMetadataProperties::HEALTH, 200);
 
         $pk = new AddActorPacket();
+
         $pk->actorUniqueId = $this->entityId;
         $pk->actorRuntimeId = $this->entityId;
         $pk->type = EntityIds::ENDER_DRAGON;
+
         $pk->position = $player->getPosition();
         $pk->motion = null;
+
         $pk->yaw = 0;
         $pk->pitch = 0;
         $pk->headYaw = 0;
+
         $pk->metadata = $metadata->getAll();
 
         $player->getNetworkSession()->sendDataPacket($pk);
     }
 
-    public function removeBossBar(Player $player): void{
+    private function removeBossBar(Player $player) : void{
 
         $pk = new RemoveActorPacket();
         $pk->actorUniqueId = $this->entityId;
