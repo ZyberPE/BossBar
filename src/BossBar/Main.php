d697aa692e31c22e1e904708a1e910b9645ff7d0
ZyberPE <zyberpe@gmail.com>
2026-03-11 16:36:30 -0400

    Update Main.php

--- src/BossBar/Main.php
+++ src/BossBar/Main.php
@@ -4,20 +4,12 @@ namespace BossBar;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\scheduler\ClosureTask;
-use pocketmine\player\Player;
-use pocketmine\entity\EntityDataHelper;
-use pocketmine\entity\EntityFactory;
-use pocketmine\entity\Location;
-use pocketmine\entity\Entity;
-use pocketmine\nbt\tag\CompoundTag;
-use pocketmine\world\World;
+use pocketmine\network\mcpe\protocol\BossEventPacket;
 use pocketmine\Server;
-
-use pocketmine\entity\Living;
 
 class Main extends PluginBase{
 
-    private array $dragons = [];
+    private int $bossId = 12345;
     private int $messageIndex = 0;
 
     protected function onEnable() : void{
@@ -25,7 +17,7 @@ class Main extends PluginBase{
 
         $interval = $this->getConfig()->get("update-time", 5) * 20;
 
-        $this->getScheduler()->scheduleRepeatingTask(new ClosureTask(function() : void{
+        $this->getScheduler()->scheduleRepeatingTask(new ClosureTask(function(): void{
             $this->updateBossBar();
         }), $interval);
     }
@@ -38,40 +30,21 @@ class Main extends PluginBase{
             $this->messageIndex = 0;
         }
 
-        $top = $messages[$this->messageIndex]["top"] ?? "";
-        $bottom = $messages[$this->messageIndex]["bottom"] ?? "";
-
-        $text = $top . "\n" . $bottom;
+        $text = $messages[$this->messageIndex];
 
         foreach(Server::getInstance()->getOnlinePlayers() as $player){
-            $this->spawnDragon($player, $text);
+
+            $pk = BossEventPacket::show(
+                $this->bossId,
+                $text,
+                1.0,
+                BossEventPacket::COLOR_PURPLE,
+                BossEventPacket::OVERLAY_PROGRESS
+            );
+
+            $player->getNetworkSession()->sendDataPacket($pk);
         }
 
         $this->messageIndex++;
-    }
-
-    private function spawnDragon(Player $player, string $text) : void{
-
-        if(isset($this->dragons[$player->getName()])){
-            $dragon = $this->dragons[$player->getName()];
-            if($dragon instanceof Entity && !$dragon->isClosed()){
-                $dragon->setNameTag($text);
-                return;
-            }
-        }
-
-        $pos = $player->getPosition()->add(0, 100, 0);
-
-        $nbt = EntityDataHelper::createBaseNBT($pos);
-
-        $dragon = EntityFactory::getInstance()->create("minecraft:ender_dragon", $pos->getWorld(), $nbt);
-
-        if($dragon !== null){
-            $dragon->setNameTag($text);
-            $dragon->setNameTagAlwaysVisible();
-            $dragon->spawnTo($player);
-
-            $this->dragons[$player->getName()] = $dragon;
-        }
     }
 }
